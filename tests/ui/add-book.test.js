dca8ea75401724b5694fcc2f8196d8f617b93ec1
violletta1 <villygeorgieva@yahoo.com>
2023-12-14 17:21:21 +0000

    change the URL in ui tests

--- tests/ui/add-book.test.js
+++ tests/ui/add-book.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add book page', async ({ page }) => {
-    await page.goto('http://localhost:8080/add-book');
+    await page.goto('https://violetta1-library-exam.onrender.com/add-book');
     const form = await page.$('form');
     expect(form).toBeTruthy();
   });
